2c741e4311a80cfe7e5f7355e8dcaab9ebf74349
nkakalov <nkakalov>
2021-01-29 17:31:14 +0300

    save2

--- startJS.js
+++ startJS.js
@@ -3,24 +3,8 @@ let input = document.querySelector('#elem1')
 let button = document.querySelector('button')
 let option = document.querySelectorAll('select option')
 input.value = select.value
-input.addEventListener('change', () => headRed())
 select.addEventListener('change', () => changeCurrentValue())
 button.addEventListener('click', () => getCurrentValue())
-
-
-
- const headRed = () => {
-     if (input.value === "") {
-         input.style.border = "2px solid red "
-         alert('empty field')
-        return;
-     }
-     else {
-         input.style.border = "2px solid grey "
-         button.disabled = false
-     }
- }
-
 
 function changeCurrentValue() {
     Array.from(select.options).forEach((item) => {
@@ -30,8 +14,18 @@ function changeCurrentValue() {
     })
 }
 
-
 function getCurrentValue(){
+    if (input.value === "") {
+        input.style.border = "2px solid red "
+        alert('empty field')
+       return;
+    }
+    else {
+        input.style.border = "2px solid grey "
+        button.disabled = false
+    }    
+    
+    
     Array.from(select.options).forEach((item) => {
         if (item.selected) {
             item.text = input.value
